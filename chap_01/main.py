d7d97140c69fb619ca5c85ac79f82f58e28c0604
kimdongheon <dongheon@gmail.com>
2025-03-10 21:05:37 +0900

    김동헌

--- chap_01/main.py
+++ chap_01/main.py
@@ -1,23 +1,20 @@
 # ======================= DEFINE ============================
-COL = 0
-
-TIMESTAMP = 0
-EVENT = 1
-MESSAGE = 2
+COL = 0 # 데이터 필드
+TIMESTAMP = 0 # 레코드[0] = 타임스탬프
+ROOTPATH = 'D:\workspace\학교\codyssey_2025_1\chap_01' # 프로젝트 루트 디렉터리
 
 # ======================== HELLO WORLD ====================================
 print('Hello World')
 
 # ========================= FILE OUT ===================================
 
-with open('mission_computer_main.log', 'r', encoding='utf-8') as file:
+with open( ROOTPATH + '\mission_computer_main.log', 'r', encoding='utf-8') as file:
     lines = file.read().split('\n')
 
 column = lines[COL].split(',')
 data = [line.split(',') for line in lines[1:] if line]
 
 time_dict = {}
-
 # >> start for <<
 for row in data:
     time = row[TIMESTAMP]
@@ -27,16 +24,19 @@ for row in data:
 # >> end for <<
 
 #out
-with open('log_analysis.md', 'w', encoding='utf-8') as f:
+with open( ROOTPATH + '\log_analysis.md', 'w', encoding='utf-8') as f:
     # >> start for <<
     for time, entries in sorted(time_dict.items()):
         f.write(f'## {time}\n')
-     
+        print(f'============== {time} ==============')
+
         # >> start for <<
         for entry in entries:
             f.write(f'- {', '.join(entry)}\n')
+            print(entry)
         # >> end for <<
-        
+
+        print('\n')
         f.write('\n') # escape
     # >> end for <<
     
